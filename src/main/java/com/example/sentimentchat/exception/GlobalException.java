375adab61cf01fb034cb5bde13c2a9bbb523cf60
vibhork85 <vibhork85@gmail.com>
2025-10-17 11:51:09 +0530

    log added in exception

--- src/main/java/com/example/sentimentchat/exception/GlobalException.java
+++ src/main/java/com/example/sentimentchat/exception/GlobalException.java
@@ -1,5 +1,6 @@
 package com.example.sentimentchat.exception;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
@@ -9,10 +10,12 @@ import java.util.HashMap;
 import java.util.Map;
 
 @ControllerAdvice
+@Slf4j
 public class GlobalException {
 
     @ExceptionHandler(UserDoesNotExistException.class)
     public ResponseEntity<Map<String, String>> handleUserDoesNotException() {
+        log.error("User does not exist exception occurred");
         Map<String, String> map = new HashMap<>();
 
         map.put("message", "user does not exist");
@@ -22,6 +25,7 @@ public class GlobalException {
 
     @ExceptionHandler(PasswordDoesNotMatchException.class)
     public ResponseEntity<Map<String, String>> handlePasswordDoesNotMatchException() {
+        log.error("Password does not match exception occurred");
         Map<String, String> map = new HashMap<>();
 
         map.put("message", "Password does not match");
@@ -31,6 +35,7 @@ public class GlobalException {
 
     @ExceptionHandler(UsernameAlreadyExistsException.class)
     public ResponseEntity<Map<String, String>> handleUsernameAlreadyExistsException(UsernameAlreadyExistsException ex) {
+        log.error("Username already exists: {}", ex.getMessage());
         Map<String, String> map = new HashMap<>();
         map.put("message", ex.getMessage());
         return new ResponseEntity<>(map, HttpStatus.CONFLICT);
@@ -38,6 +43,7 @@ public class GlobalException {
 
     @ExceptionHandler(IllegalArgumentException.class)
     public ResponseEntity<Map<String, String>> handleIllegalArgumentException(IllegalArgumentException ex) {
+        log.error("Illegal argument exception: {}", ex.getMessage());
         Map<String, String> map = new HashMap<>();
         map.put("message", ex.getMessage());
         return new ResponseEntity<>(map, HttpStatus.BAD_REQUEST);
